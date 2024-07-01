29f9b9f2af8b015e429b7450356e7f3f1df3871f
Antonio <antonio@MacBook-Pro-de-Antonio.local>
2024-07-01 14:24:53 +0200

    Added basic option menu

--- deotter.py
+++ deotter.py
@@ -1,2 +1,11 @@
 # DeOtter, the DeObfuscation tool for Cyber Security Analysts - Developed with ❤ from Spain by @HackyChucky
 print ('Hello, this is DeOtter, the DeObfuscation tool for Cyber Security Analysts - Developed with ❤ from Spain by @HackyChucky')
+
+
+def option_menu():
+    print("\nOPTIONS")
+    print("1. OPTION 1: Analyze code obfuscation technics")
+    print("2. OPTION 2: Show deobfuscated code")
+    print("3. OPTION 3: Dump deobfuscated code to file")
+    print("3. OPTION 4: Generate full report")
+    print("5. EXIT")
